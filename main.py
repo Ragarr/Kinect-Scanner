1b86d2d984bfe6e7e2743f7eb61cc4a78eb34086
Raúl Aguilar Arroyo <raulagarr@gmail.com>
2024-07-13 11:41:41 +0200

    aiuda

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from packages.kinect_interface.frame_processor import FrameProcessor
 from packages.utils import *
 import open3d as o3d
 import numpy as np
+
+import matplotlib.pyplot as plt
 cf = load_config()
 
 print("Config loaded: ")
@@ -12,11 +14,13 @@ sensor = Sensor()
 
 frame = sensor.get_rgbd_frame()
 
+plt.figure()
+plt.imshow(frame[4])
+plt.show()
 
 
 ptc = FrameProcessor.depth_image_to_pointcloud(frame, cf['pcd']['filter'], cf['pcd']['estimate_normals'], cf['pcd']['downsample_size'])
 
 
-FrameProcessor.reduce_noise(ptc, cf['pcd']['noise_eps'], cf['pcd']['min_neighbors'])
 
 o3d.visualization.draw_geometries([ptc])
--- packages/kinect_interface/sensor.py
+++ packages/kinect_interface/sensor.py
@@ -7,7 +7,12 @@ from pykinect import nui
 import numpy as np
 import threading
 from scipy.ndimage import map_coordinates
-
+import cv2
+import numpy as np
+from scipy.ndimage import affine_transform
+from scipy.optimize import minimize
+from sklearn.neighbors import KNeighborsRegressor
+np.seterr(divide='ignore', invalid='ignore')
 
 class Sensor:
     def __init__(self, resolution: str="640x480") -> None:
@@ -118,9 +123,10 @@ class Sensor:
         depth_frame = self.get_depth_frame()
         color_frame = self.get_color_frame()
         
-        color_frame = self.__zoom_image(color_frame, zoom_factor)
-        
-        rgbd_frame = np.zeros((self.height, self.width, 4), dtype=np.uint16)
+        # color_frame = self.__zoom_image(color_frame, zoom_factor)
+                
+        transofrmed_rgb_frame = np.zeros((self.height, self.width, 3), dtype=np.uint16)
+        transofrmed_depth_frame = np.zeros((self.height, self.width), dtype=np.uint16)
         
         # for each x, y coordinate in the depth frame
         for depth_y in range(self.height):
@@ -137,40 +143,97 @@ class Sensor:
                 color_y = int(color_y * y_scale)
                                
                 if color_x >= 0 and color_x < self.width and color_y >= 0 and color_y < self.height:
-                    rgbd_frame[depth_y, depth_x, 3] = depth_frame[depth_y, depth_x] 
-                    rgbd_frame[depth_y, depth_x, 2] = color_frame[color_y, color_x, 0]  
-                    rgbd_frame[depth_y, depth_x, 1] = color_frame[color_y, color_x, 1]
-                    rgbd_frame[depth_y, depth_x, 0] = color_frame[color_y, color_x, 2]
-                
-        # change BGR to RGB
-        rgbd_frame[:, :, 2] = color_frame[:, :, 0]
-        rgbd_frame[:, :, 1] = color_frame[:, :, 1]
-        rgbd_frame[:, :, 0] = color_frame[:, :, 2]
-        
-        # move just the color 10 px to the left, without reintroducting the same pixels
-        rgbd_frame[:, :, :3] = np.roll(rgbd_frame[:, :, :3], x_rolling_factor, axis=1)
-        rgbd_frame[:, :, :3] = np.roll(rgbd_frame[:, :, :3], y_rolling_factor, axis=0)
-        
-        # remove the 0 depth values and the reintruduced pixels
-        rgbd_frame[rgbd_frame[:, :, 3] == 0] = 0
-        rgbd_frame[rgbd_frame[:, :, 3] == 65535] = 0
-        
-        return rgbd_frame    
+                    transofrmed_depth_frame[depth_y, depth_x] = depth_frame[depth_y, depth_x] 
+                    transofrmed_rgb_frame[depth_y, depth_x] = color_frame[color_y, color_x][:3]
 
+        # depth_frame, _, _ = self.align_rgb_d(transofrmed_depth_frame, transofrmed_rgb_frame)
+        return np.dstack((transofrmed_rgb_frame, depth_frame))
+    
+    
+    def complete_depth_frame(self, depth_frame, k=5):
+        """Uses knn to complete the depth frame. The points with depth value 0 are replaced by the average of the k nearest neighbors.
+
+        Args:
+            depth_frame (np.ndarray): Depth frame
+            k (int, optional): Number of nearest neighbors to consider. Defaults to 5.
+        """
+        zero_indices = np.argwhere(depth_frame == 0)
+        non_zero_indices = np.argwhere(depth_frame != 0)
+        non_zero_coords = non_zero_indices[:, [1, 0]]  # Swap x, y for coordinate order
+        non_zero_values = depth_frame[depth_frame != 0]
+        
+        knn = KNeighborsRegressor(n_neighbors=k)
+        knn.fit(non_zero_coords, non_zero_values)
+        
+        zero_coords = zero_indices[:, [1, 0]]  # Swap x, y for coordinate order
+        zero_values = knn.predict(zero_coords)
+        
+        depth_frame[zero_indices[:, 0], zero_indices[:, 1]] = zero_values
+        return depth_frame
+    
+
+
+        
+    
+    def align_rgb_d(self, depth_image, rgb_image ):
+        """Aligns the depth image with the rgb image using ORB feature matching and homography transformation.
+
+        Args:
+            img_depth (np.ndarray): Depth image
+            img_rgb (np.ndarray): RGB image
+            
+        Returns:
+            np.ndarray: Aligned depth image
+            np.ndarray: RGB image
+            np.ndarray: Transformation matrix
+        """    
+        # Función para calcular la Entropía de una imagen
+        def image_entropy(image):
+            hist = np.histogram(image, bins=256, range=(0, 255))[0]
+            hist = hist / np.sum(hist)  # Normalizar el histograma
+            return -np.sum(hist * np.log(hist + 1e-15))  # Evitar log(0)
+
+        # Función para calcular la Información Mutua entre dos imágenes
+        def mutual_information(image1, image2):
+            hist_2d, _, _ = np.histogram2d(image1.flatten(), image2.flatten(), bins=256, range=[[0, 255], [0, 255]])
+            return image_entropy(image1) + image_entropy(image2) - image_entropy(hist_2d)
+
+        # Función para optimizar la transformación usando Información Mutua
+        def optimize_transform(image1, image2, initial_params):
+            def loss(params):
+                transformed = affine_transform(image2, params.reshape((2, 3)))
+                return -mutual_information(image1, transformed)
+
+            result = minimize(loss, initial_params, method='Nelder-Mead')
+            return result.x
+
+
+        def initial_transform():
+            return np.array([1.0, 0.0, 0.0, 0.0, 1.0, 0.0])  # Sin transformación inicial
+
+        
+        # relleando los valores de la imagen de profundidad
+        depth_image=self.complete_depth_frame(depth_image)
+
+        img_color_gray = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2GRAY)
+        print("Optimizing depth transform...")
+        params = optimize_transform(img_color_gray, depth_image, initial_transform())
+        print("Transforming depth image...")
+        aligned_depth = affine_transform(depth_image, params.reshape((2, 3)))
+        print("Done")
+        return aligned_depth, rgb_image, params
+        
+        
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     sensor = Sensor()
     rgbd_frame = sensor.get_rgbd_frame()
     # show color and depth frames
     plt.figure()
-    plt.imshow(rgbd_frame[:, :, :3])
+    plt.subplot(121)
+    plt.imshow(rgbd_frame[..., :3])
     plt.title("Color Frame")
-    plt.axis("off")
-    plt.figure()
-    plt.imshow(rgbd_frame[:, :, 3], cmap="gray")
+    plt.subplot(122)
+    plt.imshow(rgbd_frame[..., 3], cmap='gray')
     plt.title("Depth Frame")
-    plt.axis("off")
     plt.show()
-    
-    
-    
